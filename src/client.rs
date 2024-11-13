305d343faca067787b4723cc1143673f4c196075
JIEEEN <tjtpp009@korea.ac.kr>
2024-11-13 11:37:43 +0000

    add client info transfer

--- src/client.rs
+++ src/client.rs
@@ -11,9 +11,16 @@ fn main() {
     println!("Server address: {}", server_addr);
 
     let mut stream = TcpStream::connect(server_addr).expect("Could not connect to server");
-    let mut buff = [0u8; 1024];
 
     let private_ip = stream.local_addr().unwrap().ip();
     let private_port = stream.local_addr().unwrap().port();
-    let _ = stream.write(format!("{}:{}", private_ip, private_port).as_bytes());
+    let client_info = format!("{}:{}", private_ip, private_port);
+    stream.write_all(client_info.as_bytes()).unwrap();
+
+    let mut buff = [0u8; 1024];
+    let len = stream.read(&mut buff).unwrap();
+    println!(
+        "another client info: \n{}",
+        String::from_utf8_lossy(&buff[..len])
+    );
 }
--- src/server.rs
+++ src/server.rs
@@ -1,6 +1,7 @@
 use lazy_static::lazy_static;
+use std::io::Write;
 use std::sync::Mutex;
-use std::{io::Read, net::TcpListener};
+use std::{io::Read, net::TcpListener, net::TcpStream};
 
 lazy_static! {
     static ref A_IP_PRIVATE_ADDR: Mutex<Option<String>> = Default::default();
@@ -16,14 +17,13 @@ fn main() {
         .expect("Could not bind to address");
     println!("Listening on port: {}", LISTEN_PORT);
     let mut buff = [0u8; 1024];
-
-    let mut connect_peer: bool = false;
+    let mut socket_vec: Vec<TcpStream> = Default::default();
 
     while let Ok((stream, _)) = listener.accept() {
         let mut stream = stream.try_clone().expect("Could not clone stream");
+        socket_vec.push(stream.try_clone().unwrap());
 
         let data_len = stream.read(&mut buff).unwrap();
-        println!("{}", String::from_utf8_lossy(&buff[..data_len]));
         let mut lock_a_pub = A_IP_PUBLIC_ADDR.lock().unwrap();
 
         match &*lock_a_pub {
@@ -45,9 +45,9 @@ fn main() {
                             lock_b_pub.clone().unwrap(),
                             lock_b_pri.clone().unwrap()
                         );
+                        break;
                     }
                 }
-                connect_peer = true;
             }
             None => {
                 let pub_addr = String::from_utf8_lossy(&buff[..data_len]);
@@ -65,20 +65,25 @@ fn main() {
                 );
             }
         }
-
-        if !connect_peer {
-            continue;
-        }
-
-        println!(
-            "{}\n{}\n",
-            A_IP_PRIVATE_ADDR.lock().unwrap().take().unwrap(),
-            A_IP_PUBLIC_ADDR.lock().unwrap().take().unwrap()
-        );
-        println!(
-            "{}\n{}\n",
-            B_IP_PRIVATE_ADDR.lock().unwrap().take().unwrap(),
-            B_IP_PUBLIC_ADDR.lock().unwrap().take().unwrap()
-        );
     }
+
+    let a_info = format!(
+        "{}\n{}",
+        A_IP_PRIVATE_ADDR.lock().unwrap().take().unwrap(),
+        A_IP_PUBLIC_ADDR.lock().unwrap().take().unwrap()
+    );
+    let b_info = format!(
+        "{}\n{}",
+        B_IP_PRIVATE_ADDR.lock().unwrap().take().unwrap(),
+        B_IP_PUBLIC_ADDR.lock().unwrap().take().unwrap()
+    );
+
+    let mut client_a = &socket_vec[0];
+    let mut client_b = &socket_vec[1];
+
+    client_a
+        .write_all(b_info.as_bytes()).unwrap();
+
+    client_b
+        .write_all(a_info.as_bytes()).unwrap();
 }
