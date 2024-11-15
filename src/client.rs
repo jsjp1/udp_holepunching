92c92bd1e9d81b23f52845ba5ae22be8e5576197
JIEEEN <tjtpp009@korea.ac.kr>
2024-11-15 13:14:08 +0900

    add lan, wan

--- src/client.rs
+++ src/client.rs
@@ -1,8 +1,41 @@
 use dotenv::dotenv;
 use std::{
     io::{Read, Write},
-    net::TcpStream,
+    net::{SocketAddr, TcpStream, UdpSocket},
+    time::Duration,
 };
+
+static READ_TIMEOUT: u64 = 500;
+
+fn attempt_connection(socket: &UdpSocket, peer_addr: SocketAddr, timeout: Duration) -> bool {
+    let start_time = std::time::Instant::now();
+    while start_time.elapsed() < timeout {
+        let msg = b"Hello!";
+        match socket.send_to(msg, peer_addr) {
+            Ok(_) => {
+                println!("Sent packet to {}", peer_addr);
+
+                let mut buff = [0u8; 1024];
+                match socket.recv_from(&mut buff) {
+                    Ok((amt, src)) => {
+                        println!(
+                            "Received from {}: {}",
+                            src,
+                            String::from_utf8_lossy(&buff[..amt])
+                        );
+                        return true;
+                    }
+                    Err(_) => continue,
+                }
+            }
+            Err(e) => {
+                eprintln!("Failed to send to {}: {}", peer_addr, e);
+            }
+        }
+        std::thread::sleep(Duration::from_millis(100));
+    }
+    false
+}
 
 fn main() {
     dotenv().ok();
@@ -19,8 +52,65 @@ fn main() {
 
     let mut buff = [0u8; 1024];
     let len = stream.read(&mut buff).unwrap();
-    println!(
-        "another client info: \n{}",
-        String::from_utf8_lossy(&buff[..len])
-    );
+    let buff_str = String::from_utf8_lossy(&buff[..len]);
+    println!("another client info: \n{}", buff_str);
+
+    let mut parts = buff_str.splitn(2, "\n");
+    let peer_addr_pub = parts.next().unwrap_or_default().to_string();
+    let peer_addr_priv = parts.next().unwrap_or_default().to_string();
+
+    // First, find peer from LAN
+    let udp_socket = UdpSocket::bind(format!("0.0.0.0:{}", private_port)).unwrap();
+    udp_socket
+        .set_read_timeout(Some(Duration::from_millis(READ_TIMEOUT)))
+        .unwrap();
+
+    // listening thread detach from main thread
+    let receive_socket = udp_socket.try_clone().unwrap();
+    std::thread::spawn(move || {
+        let mut buff = [0u8; 1024];
+        loop {
+            match receive_socket.recv_from(&mut buff) {
+                Ok((amt, src)) => {
+                    let message = String::from_utf8_lossy(&buff[..amt]);
+                    println!("Message from {}: {}", src, message);
+                }
+                Err(_) => {}
+            }
+        }
+    });
+
+    let message = "LAN Test Message";
+    match udp_socket.send_to(message.as_bytes(), &peer_addr_priv) {
+        Ok(_) => {
+            println!("Successfully found a peer on the LAN");
+            loop {
+                let mut message = String::new();
+
+                print!("Write Message to Peer: ");
+                std::io::stdout().flush().unwrap();
+
+                std::io::stdin().read_line(&mut message).unwrap();
+                udp_socket
+                    .send_to(message.as_bytes(), &peer_addr_priv)
+                    .unwrap();
+            }
+        }
+        Err(e) => {
+            println!("Cannot find peer on the LAN: {}", e);
+
+            // Next, find peer from WAN
+            loop {
+                let mut message = String::new();
+
+                print!("Write Message to Peer: ");
+                std::io::stdout().flush().unwrap();
+
+                std::io::stdin().read_line(&mut message).unwrap();
+                udp_socket
+                    .send_to(message.as_bytes(), &peer_addr_pub)
+                    .unwrap();
+            }
+        }
+    }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,8 @@
 use lazy_static::lazy_static;
-use std::io::Write;
-use std::sync::Mutex;
-use std::{io::Read, net::TcpListener, net::TcpStream};
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::sync::{Arc, Mutex};
+use std::thread;
 
 lazy_static! {
     static ref A_IP_PRIVATE_ADDR: Mutex<Option<String>> = Default::default();
@@ -13,11 +14,11 @@ lazy_static! {
 static LISTEN_PORT: u16 = 5001;
 
 fn main() {
-    let listener = TcpListener::bind(format!("{}:{}", "0.0.0.0", LISTEN_PORT))
-        .expect("Could not bind to address");
+    let listener =
+        TcpListener::bind(format!("0.0.0.0:{}", LISTEN_PORT)).expect("Could not bind to address");
     println!("Listening on port: {}", LISTEN_PORT);
     let mut buff = [0u8; 1024];
-    let mut socket_vec: Vec<TcpStream> = Default::default();
+    let mut socket_vec = Vec::new();
 
     while let Ok((stream, _)) = listener.accept() {
         let mut stream = stream.try_clone().expect("Could not clone stream");
@@ -39,7 +40,7 @@ fn main() {
                         let mut lock_b_pri = B_IP_PRIVATE_ADDR.lock().unwrap();
                         *lock_b_pri = Some(format!("{}:{}", client_info.ip(), client_info.port()));
 
-                        println!("B join to server");
+                        println!("B joined the server");
                         println!(
                             "{}\n{}\n",
                             lock_b_pub.clone().unwrap(),
@@ -57,7 +58,7 @@ fn main() {
                 let mut lock_a_pri = A_IP_PRIVATE_ADDR.lock().unwrap();
                 *lock_a_pri = Some(format!("{}:{}", client_info.ip(), client_info.port()));
 
-                println!("A join to server");
+                println!("A joined the server");
                 println!(
                     "{}\n{}\n",
                     lock_a_pub.clone().unwrap(),
@@ -67,6 +68,7 @@ fn main() {
         }
     }
 
+    // Send peer's address to each other
     let a_info = format!(
         "{}\n{}",
         A_IP_PRIVATE_ADDR.lock().unwrap().take().unwrap(),
@@ -78,12 +80,69 @@ fn main() {
         B_IP_PUBLIC_ADDR.lock().unwrap().take().unwrap()
     );
 
-    let mut client_a = &socket_vec[0];
-    let mut client_b = &socket_vec[1];
+    let client_a = Arc::new(Mutex::new(socket_vec[0].try_clone().unwrap()));
+    let client_b = Arc::new(Mutex::new(socket_vec[1].try_clone().unwrap()));
 
     client_a
-        .write_all(b_info.as_bytes()).unwrap();
-
+        .lock()
+        .unwrap()
+        .write_all(b_info.as_bytes())
+        .unwrap();
     client_b
-        .write_all(a_info.as_bytes()).unwrap();
+        .lock()
+        .unwrap()
+        .write_all(a_info.as_bytes())
+        .unwrap();
+
+    // Start threads to relay data between A and B
+    let client_a_clone = Arc::clone(&client_a);
+    let client_b_clone = Arc::clone(&client_b);
+
+    // A -> B
+    let handle_a_to_b = thread::spawn(move || {
+        let mut buffer = [0u8; 1024];
+        loop {
+            match client_a_clone.lock().unwrap().read(&mut buffer) {
+                Ok(size) if size > 0 => {
+                    if client_b_clone
+                        .lock()
+                        .unwrap()
+                        .write_all(&buffer[..size])
+                        .is_err()
+                    {
+                        println!("Failed to write data from A to B");
+                        break;
+                    }
+                }
+                Ok(_) => break,
+                Err(_) => {
+                    println!("Connection closed by A");
+                    break;
+                }
+            }
+        }
+    });
+
+    // B -> A
+    let handle_b_to_a = thread::spawn(move || {
+        let mut buffer = [0u8; 1024];
+        loop {
+            match client_b.lock().unwrap().read(&mut buffer) {
+                Ok(size) if size > 0 => {
+                    if client_a.lock().unwrap().write_all(&buffer[..size]).is_err() {
+                        println!("Failed to write data from B to A");
+                        break;
+                    }
+                }
+                Ok(_) => break,
+                Err(_) => {
+                    println!("Connection closed by B");
+                    break;
+                }
+            }
+        }
+    });
+
+    handle_a_to_b.join().unwrap();
+    handle_b_to_a.join().unwrap();
 }
