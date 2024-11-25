39d3b1e442aa1346c72cf399c4e4cbcf91d3df84
JIEEEN <tjtpp009@korea.ac.kr>
2024-11-26 00:07:05 +0900

    add env_example, mod process

--- src/client.rs
+++ src/client.rs
@@ -2,17 +2,19 @@ use dotenv::dotenv;
 use std::{
     io::{Read, Write},
     net::{TcpStream, UdpSocket},
+    sync::{Arc, Mutex},
     time::Duration,
 };
 
-static READ_TIMEOUT: u64 = 500;
+static READ_TIMEOUT: u64 = 1000;
 
 fn main() {
     dotenv().ok();
+    let lan_test_success;
 
     let (tx, rx) = std::sync::mpsc::channel();
 
-    let _ = std::thread::spawn(move || loop {
+    std::thread::spawn(move || loop {
         match rx.recv() {
             Ok(msg) => {
                 println!("{}", msg);
@@ -24,7 +26,7 @@ fn main() {
     });
 
     let server_addr = std::env::var("SERVER_ADDR").expect("SERVER_ADDR must be set");
-    let _ = tx.send(format!("Server addres: {}", server_addr));
+    tx.send(format!("Server addres: {}", server_addr)).unwrap();
 
     let mut stream = TcpStream::connect(server_addr).expect("Could not connect to server");
 
@@ -36,66 +38,75 @@ fn main() {
     let mut buff = [0u8; 1024];
     let len = stream.read(&mut buff).unwrap();
     let buff_str = String::from_utf8_lossy(&buff[..len]);
-    let _ = tx.send(format!("another client info: \n{}", buff_str));
+    tx.send(format!("another client info: \n{}", buff_str))
+        .unwrap();
 
     let mut parts = buff_str.splitn(2, "\n");
     let peer_addr_pub = parts.next().unwrap_or_default().to_string();
     let peer_addr_priv = parts.next().unwrap_or_default().to_string();
 
-    // First, find peer from LAN
+    // create udp socket
     let udp_socket = UdpSocket::bind(format!("0.0.0.0:{}", private_port)).unwrap();
     udp_socket
         .set_read_timeout(Some(Duration::from_millis(READ_TIMEOUT)))
         .unwrap();
 
     // listening thread detach from main thread
-    let receive_socket = udp_socket.try_clone().unwrap();
+    let socket_lock = Arc::new(Mutex::new(udp_socket));
+
+    // test LAN communication
+    let socket = socket_lock.lock().unwrap().try_clone().unwrap();
+    socket
+        .send_to("LAN test message".as_bytes(), &peer_addr_priv)
+        .unwrap();
+
+    let mut buff = [0u8; 1024];
+    let receive_socket = socket_lock.lock().unwrap().try_clone().unwrap();
+    match receive_socket.recv_from(&mut buff) {
+        Ok((amt, src)) => {
+            let message: std::borrow::Cow<'_, str> = String::from_utf8_lossy(&buff[..amt]);
+            tx.send(format!("Message from {}: {}", src, message))
+                .unwrap();
+
+            lan_test_success = true;
+        }
+        Err(e) => {
+            tx.send(format!("{}", e)).unwrap();
+            lan_test_success = false;
+        }
+    }
+
+    // create Receive only thread
     let tx_clone = tx.clone();
-    std::thread::spawn(move || {
+    let _ = std::thread::spawn(move || {
         let mut buff = [0u8; 1024];
+        let receive_socket = socket_lock.lock().unwrap().try_clone().unwrap();
         loop {
             match receive_socket.recv_from(&mut buff) {
                 Ok((amt, src)) => {
-                    let message = String::from_utf8_lossy(&buff[..amt]);
-                    let _ = tx_clone.send(format!("Message from {}: {}", src, message));
+                    let message: std::borrow::Cow<'_, str> = String::from_utf8_lossy(&buff[..amt]);
+                    tx_clone
+                        .send(format!("Message from {}: {}", src, message))
+                        .unwrap();
+                    tx_clone
+                        .send("Write message to peer: ".to_string())
+                        .unwrap();
                 }
                 Err(_) => {}
             }
         }
     });
 
-    let message = "LAN Test Message";
-    let tx_clone = tx.clone();
-    match udp_socket.send_to(message.as_bytes(), &peer_addr_priv) {
-        Ok(_) => {
-            let _ = tx_clone.send(format!("Successfully found a peer on the LAN"));
-            loop {
-                let mut message = String::new();
+    loop {
+        tx.send("Write message to peer: ".to_string()).unwrap();
 
-                let _ = tx_clone.send(format!("Write Message to Peer: "));
-                std::io::stdout().flush().unwrap();
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
 
-                std::io::stdin().read_line(&mut message).unwrap();
-                udp_socket
-                    .send_to(message.as_bytes(), &peer_addr_priv)
-                    .unwrap();
-            }
-        }
-        Err(e) => {
-            let _ = tx_clone.send(format!("Cannot find peer on the LAN: {}", e));
-
-            // Next, find peer from WAN
-            loop {
-                let mut message = String::new();
-
-                let _ = tx_clone.send(format!("Write Message to Peer: "));
-                std::io::stdout().flush().unwrap();
-
-                std::io::stdin().read_line(&mut message).unwrap();
-                udp_socket
-                    .send_to(message.as_bytes(), &peer_addr_pub)
-                    .unwrap();
-            }
+        if lan_test_success == true {
+            socket.send_to(input.as_bytes(), &peer_addr_priv).unwrap();
+        } else {
+            socket.send_to(input.as_bytes(), &peer_addr_pub).unwrap();
         }
     }
 }
