6039e0a5ef510556fc4558d1910a5cf8fe2b5377
JIEEEN <tjtpp009@korea.ac.kr>
2024-11-25 18:34:07 +0900

    add print only thread

--- src/client.rs
+++ src/client.rs
@@ -1,47 +1,30 @@
 use dotenv::dotenv;
 use std::{
     io::{Read, Write},
-    net::{SocketAddr, TcpStream, UdpSocket},
+    net::{TcpStream, UdpSocket},
     time::Duration,
 };
 
 static READ_TIMEOUT: u64 = 500;
 
-fn attempt_connection(socket: &UdpSocket, peer_addr: SocketAddr, timeout: Duration) -> bool {
-    let start_time = std::time::Instant::now();
-    while start_time.elapsed() < timeout {
-        let msg = b"Hello!";
-        match socket.send_to(msg, peer_addr) {
-            Ok(_) => {
-                println!("Sent packet to {}", peer_addr);
-
-                let mut buff = [0u8; 1024];
-                match socket.recv_from(&mut buff) {
-                    Ok((amt, src)) => {
-                        println!(
-                            "Received from {}: {}",
-                            src,
-                            String::from_utf8_lossy(&buff[..amt])
-                        );
-                        return true;
-                    }
-                    Err(_) => continue,
-                }
-            }
-            Err(e) => {
-                eprintln!("Failed to send to {}: {}", peer_addr, e);
-            }
-        }
-        std::thread::sleep(Duration::from_millis(100));
-    }
-    false
-}
-
 fn main() {
     dotenv().ok();
 
+    let (tx, rx) = std::sync::mpsc::channel();
+
+    let _ = std::thread::spawn(move || loop {
+        match rx.recv() {
+            Ok(msg) => {
+                println!("{}", msg);
+            }
+            Err(err) => {
+                eprintln!("{}", err);
+            }
+        }
+    });
+
     let server_addr = std::env::var("SERVER_ADDR").expect("SERVER_ADDR must be set");
-    println!("Server address: {}", server_addr);
+    let _ = tx.send(format!("Server addres: {}", server_addr));
 
     let mut stream = TcpStream::connect(server_addr).expect("Could not connect to server");
 
@@ -53,7 +36,7 @@ fn main() {
     let mut buff = [0u8; 1024];
     let len = stream.read(&mut buff).unwrap();
     let buff_str = String::from_utf8_lossy(&buff[..len]);
-    println!("another client info: \n{}", buff_str);
+    let _ = tx.send(format!("another client info: \n{}", buff_str));
 
     let mut parts = buff_str.splitn(2, "\n");
     let peer_addr_pub = parts.next().unwrap_or_default().to_string();
@@ -67,13 +50,14 @@ fn main() {
 
     // listening thread detach from main thread
     let receive_socket = udp_socket.try_clone().unwrap();
+    let tx_clone = tx.clone();
     std::thread::spawn(move || {
         let mut buff = [0u8; 1024];
         loop {
             match receive_socket.recv_from(&mut buff) {
                 Ok((amt, src)) => {
                     let message = String::from_utf8_lossy(&buff[..amt]);
-                    println!("Message from {}: {}", src, message);
+                    let _ = tx_clone.send(format!("Message from {}: {}", src, message));
                 }
                 Err(_) => {}
             }
@@ -81,13 +65,14 @@ fn main() {
     });
 
     let message = "LAN Test Message";
+    let tx_clone = tx.clone();
     match udp_socket.send_to(message.as_bytes(), &peer_addr_priv) {
         Ok(_) => {
-            println!("Successfully found a peer on the LAN");
+            let _ = tx_clone.send(format!("Successfully found a peer on the LAN"));
             loop {
                 let mut message = String::new();
 
-                print!("Write Message to Peer: ");
+                let _ = tx_clone.send(format!("Write Message to Peer: "));
                 std::io::stdout().flush().unwrap();
 
                 std::io::stdin().read_line(&mut message).unwrap();
@@ -97,13 +82,13 @@ fn main() {
             }
         }
         Err(e) => {
-            println!("Cannot find peer on the LAN: {}", e);
+            let _ = tx_clone.send(format!("Cannot find peer on the LAN: {}", e));
 
             // Next, find peer from WAN
             loop {
                 let mut message = String::new();
 
-                print!("Write Message to Peer: ");
+                let _ = tx_clone.send(format!("Write Message to Peer: "));
                 std::io::stdout().flush().unwrap();
 
                 std::io::stdin().read_line(&mut message).unwrap();
